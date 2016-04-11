89cce8e2ef49900e1a143f321f57720983e5a2b7
pepocivs <tcbpepo@gmail.com>
2016-04-11 16:07:44 +0200

    indexing new modules

--- src/types/index.js
+++ src/types/index.js
@@ -1,9 +1,11 @@
 export default ngModule => {
   require('./checkbox')(ngModule);
   require('./multiCheckbox')(ngModule);
+  require('./multiInput')(ngModule);
   require('./input')(ngModule);
-  require('./koaCustom')(ngModule);
+  require('./datePicker')(ngModule);
   require('./radio')(ngModule);
   require('./select')(ngModule);
   require('./textarea')(ngModule);
+  require('./wysiwyg')(ngModule);
 };
