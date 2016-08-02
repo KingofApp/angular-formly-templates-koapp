41504ce5b57372755582e69c76902eb08fa5ae01
pepocivs <tcbpepo@gmail.com>
2016-08-02 11:24:54 +0200

    bump version

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ try {
   Package.describe({
     name: 'formly:angular-formly-templates-koapp',
     summary: '(official): Koapp templates for angular-formly',
-    version: '1.0.24',
+    version: '1.0.25',
     git: 'https://github.com/formly-js/angular-formly-templates-koapp.git',
   })
 
